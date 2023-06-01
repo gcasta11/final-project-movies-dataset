9a2971207a84b2b0d264e851a15515e225384b51
ayudha00 <ayudha@uw.edu>
2023-05-31 22:36:19 -0700

    update changes

--- server.R
+++ server.R
@@ -11,14 +11,6 @@ avg_rating <- movies_df %>%
 
 
 server <- function(input, output){
-
-  output$year_range <- renderText(input$movie_year)
-  
-  # Generate a plot of the data ----
-  # Also uses the inputs to build the plot label. Note that the
-  # dependencies on the inputs and the data reactive expression are
-  # both tracked, and all expressions are called in the sequence
-  # implied by the dependency graph.
   output$plot <- renderPlotly({
     # line graph
     avg_rating_plot <- ggplot(data = avg_rating) +
@@ -32,36 +24,3 @@ server <- function(input, output){
     return(avg_rating_plotly)
   })
 }
-
-
-movies_df <- read.csv("https://raw.githubusercontent.com/info-201b-sp23/exploratory-analysis-ayudha00/main/IMDB-Movie-Data.csv?token=GHSAT0AAAAAACAWKOPLZDDCEALPSFWLNT5SZDYGEDQ")
-
-library(shiny)
-
-ui <- fluidPage(
-  # App title ----
-  titlePanel("Exploring IMDb Hollywood Movies from 2006 to 2016"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Slider for the number of observations to generate ----
-      sliderInput("movie_year",
-                  "Year:",
-                  value = c(min(movies_df$Year), max(movies_df$Year)),
-                  min = min(movies_df$Year),
-                  max = max(movies_df$Year),
-                  step = 1)
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      plotlyOutput(outputId = "plot")
-      
-    )
-  )
-)
--- ui.R
+++ ui.R
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
 movies_df <- read.csv("https://raw.githubusercontent.com/info-201b-sp23/exploratory-analysis-ayudha00/main/IMDB-Movie-Data.csv?token=GHSAT0AAAAAACAWKOPLZDDCEALPSFWLNT5SZDYGEDQ")
-
-library(shiny)
-
-ui <- fluidPage(
-  # App title ----
-  titlePanel("Exploring IMDb Hollywood Movies from 2006 to 2016"),
   
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Slider for the number of observations to generate ----
-      sliderInput("movie_year",
-                  "Year:",
-                  value = c(min(movies_df$Year), max(movies_df$Year)),
-                  min = min(movies_df$Year),
-                  max = max(movies_df$Year),
-                  step = 1)
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      plotlyOutput(outputId = "plot")
-      
-    )
-  )
-=======
 library(shiny)
 ui <- navbarPage(
   title = "Exploring IMDb HollyWood Movies from 2006 to 2016",
@@ -168,20 +138,23 @@ ui <- navbarPage(
                  "summary features we wanted to study, in hopes of finding the ",
                  "connection between the popularity of the movie, its director, ",
                  "and the most highly ranked genre. We can observe that the most ",
-                 "highly ranked genre is the "Thriller" category, and Adam ",
+                 "highly ranked genre is the Thriller category, and Adam ",
                  "Wingard is the director most frequently seen in this dataset.")
-                 
              )
            )
-           )
-  )
-  tabPanel("Interactive Page 1"),
-  fluidPage(
-    mainPanel(
+         )
+  ),
+  tabPanel("Interactive Page 1",
+    fluidPage(
+      mainPanel(
+        sliderInput("movie_year",
+                    "Year:",
+                    value = c(min(movies_df$Year), max(movies_df$Year)),
+                    min = min(movies_df$Year),
+                    max = max(movies_df$Year),
+                    step = 1),
+        plotlyOutput(outputId = "plot")
+      )
     )
-    
   )
-  
-  
->>>>>>> 7a190b86c3c787bf84a1febfe3452f4dc3645026
 )
